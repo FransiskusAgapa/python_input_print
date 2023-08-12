5e21ae954add9491b6bcb4003e26d9a0409deeb7
FransXave <santhothosa@gmail.com>
2023-08-12 13:17:01 -0700

    new pet's and person' methods created

--- input_print.py
+++ input_print.py
@@ -16,14 +16,14 @@ class Pet:
     def eat(self):
         self.health += 10
         self.energy += 10
-    
-    def sleep(self):
-        self.health += 15
-        self.energy += 15
 
     def play(self):
         self.energy -= 10
         self.health += 10
+    
+    def sleep(self):
+        self.health += 15
+        self.energy += 15
 
 class Person:
     def __init__(self,f_name,l_name,pet_name,pet_tricks,pet_food):
@@ -32,18 +32,30 @@ class Person:
         self.pet = Pet(pet_name,pet_tricks,pet_food)
     
     def go_walk(self):
-        print(f"{self.f_name} walks with their pet, {self.pet.name}!")
+        print(f"\n{self.f_name} walks with their pet, {self.pet.name}!")
         return self
-
+    
+    def go_feed(self):
+        print(f"\n{self.f_name} feeds {self.pet.name} with {self.pet.food}!")
+        return self
+    
+    def go_play(self):
+        print(f"\n{self.f_name} plays {self.pet.tricks} tricks with {self.pet.name}!")
+        return self
+    
+    def go_sleep(self):
+        print(f"\n{self.f_name} usually sleeps with {self.pet.name} on the couch!")
+        return self
+    
 print("\n========================================")
-f_name = input("Hi, I am bot! What is your first name: ")
-l_name = input("What is your last name: ")
-pet_name = input("What is your pet name: ") # assuming this person has a pet / with a pet
-pet_tricks = input("What is your pet's favorite trick: ")
-pet_food = input("What is your favorite food: ")
+f_name = input("Hi, I am bot! What is your first name: ").capitalize()
+l_name = input("What is your last name: ").capitalize()
+pet_name = input("What is your pet name: ").capitalize() # assuming this person has a pet/with a pet
+pet_tricks = input("What is your pet's favorite trick: ").capitalize()
+pet_food = input("What is your favorite food: ").capitalize()
 
 person_one = Person(f_name,l_name,pet_name,pet_tricks,pet_food)
-person_one.go_walk()
+person_one.go_walk().go_feed().go_play().go_sleep()
 print("========================================")
 
 update_data = input("\n> Would you like to update the data: ").lower()
@@ -53,16 +65,14 @@ while(update_data == "y" or len(update_data) > 1):
 
     if(update_data == "y"):
         print("\n========================================")
-        f_name = input("Hi again, I am bot! What is your first name: ")
-        l_name = input("What is your last name: ")
-        pet_name = input("What is your pet name: ") # assuming this person has a pet / with a pet
-        pet_tricks = input("What is your pet's favorite trick: ")
-        pet_food = input("What is your favorite food: ")
-        person_one.go_walk()
-        print("========================================")
-
+        f_name = input("Hi again, I am bot! What is your first name: ").capitalize()
+        l_name = input("What is your last name: ").capitalize()
+        pet_name = input("What is your pet name: ").capitalize() # assuming this person has a pet / with a pet
+        pet_tricks = input("What is your pet's favorite trick: ").capitalize()
+        pet_food = input("What is your favorite food: ").capitalize()
         person_one = Person(f_name,l_name,pet_name,pet_tricks,pet_food)
-        person_one.go_walk()
+        person_one.go_walk().go_feed().go_play().go_sleep()
+        print("========================================")
 
     update_data = input("\n> Would you like to update the data: ").lower()
     
