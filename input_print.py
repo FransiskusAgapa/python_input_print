38bc8259244d02c4330bed46f76f4b6bc23df538
FransXave <santhothosa@gmail.com>
2023-08-12 11:16:39 -0700

    class nameupdated

--- input_print.py
+++ input_print.py
@@ -8,22 +8,30 @@ class Pet:
         self.food = food
         self.health = 0 
         self.energy = 0
+    
+    def walk(self):
+        self.energy += 5
+        self.health += 5
 
     def eat(self):
         self.health += 10
         self.energy += 10
     
     def sleep(self):
+        self.health += 15
         self.energy += 15
-        self.energy += 15
-    
+
     def play(self):
         self.energy -= 10
         self.health += 10
 
-class Ninja:
+class Person:
     def __init__(self,f_name,l_name,pet,pet_tricks,pet_food):
         self.f_name = f_name
         self.l_name = l_name
         self.pet = Pet(pet,pet_tricks,pet_food)
     
+    def go_walk(self):
+        print(f"{self.f_name} walks with their pet {self.pet}")
+        return self
+
